80c9613fb595b01e7c841a6d7aaebfc1d9c5952e
Sameer Kumar Yadav <sameerkumyadav@gmail.com>
2026-08-17 17:42:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await fetch('http://localhost:5000/chat', {
+      const res = await fetch('https://chatbot-backend-teta.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: input, sessionId })
